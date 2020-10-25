f11317f30489e86e3f7aaae69aaffd0ee8132ec8
yusuf3321 <65294604+yusuf3321@users.noreply.github.com>
2020-10-25 16:23:42 +0300

    Add files via upload
    C# && GEOMETRİ

---
+++ AÇI DÖNÜŞÜM.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EĞİTİM
+{
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(" DERECE GİRİNİZ");
+            float x = Convert.ToInt32(Console.ReadLine());
+
+            float Radyan = (x / 180);
+
+            Console.WriteLine("Radyan cinsinden = " + Radyan);
+
+            Console.ReadKey();
+
+            float Gradyan = (x / 180) * 200;
+
+            Console.WriteLine("Gradyan cinsinden = " + (Gradyan));
+           
+            Console.ReadKey();
+        }
+      
+
+    }
+}
+        
+    
+
+
+
+        
